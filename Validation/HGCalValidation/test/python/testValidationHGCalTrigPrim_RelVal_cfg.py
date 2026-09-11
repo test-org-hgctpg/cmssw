32634a15d5ed7a0a282b50f72ba3c8b682600ead
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2026-09-11 17:18:38 +0200

    Testing config from GitHub

--- Validation/HGCalValidation/test/python/testValidationHGCalTrigPrim_RelVal_cfg.py
+++ Validation/HGCalValidation/test/python/testValidationHGCalTrigPrim_RelVal_cfg.py
@@ -100,7 +100,7 @@ process.hgcalValidationTPG_step = cms.Path(process.runHGCALValidationTPG)
 #process.dqmSaver.workflow = '/validation/' + 'HGCAL' + '/TPG'
 #process.dqmsave_step = cms.Path(process.dqmSaver)
 
-# NEW added
+# Testing
 process.onlineSaver = cms.EDAnalyzer("DQMFileSaverOnline",
     producer = cms.untracked.string('DQM'),
     path = cms.untracked.string('./'),
